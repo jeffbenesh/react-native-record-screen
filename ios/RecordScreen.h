669e9d96e4191400e5cca4815ae82ac3e3faf8c8
jeffrey.b <jeffrey.b@esh.com>
2025-11-02 09:37:41 +0200

    chore: refactor RecordScreen to enhance recording capabilities and improve audio handling

--- ios/RecordScreen.h
+++ ios/RecordScreen.h
@@ -1,11 +1,23 @@
 #import <React/RCTBridgeModule.h>
-#import <React/RCTEventEmitter.h>
+#import <ReplayKit/ReplayKit.h>
+#import <AVFoundation/AVFoundation.h>
 
-@interface RecordScreen : RCTEventEmitter <RCTBridgeModule>
-@property (nonatomic, assign) int screenWidth;
-@property (nonatomic, assign) int screenHeight;
-@property (nonatomic, assign) BOOL enableMic;
-@property (nonatomic, assign) int bitrate;
-@property (nonatomic, assign) int fps;
+@interface RecordScreen : NSObject <RCTBridgeModule>
+
+    @property (strong, nonatomic) RPScreenRecorder *screenRecorder;
+    @property (strong, nonatomic) AVAssetWriterInput *videoInput;
+    @property (strong, nonatomic) AVAssetWriterInput *audioInput;
+    @property (strong, nonatomic) AVAssetWriterInput *micInput;
+    @property (assign, nonatomic) int screenWidth;
+    @property (assign, nonatomic) int screenHeight;
+    @property (assign, nonatomic) BOOL enableMic;
+    @property (assign, nonatomic) int fps;
+    @property (assign, nonatomic) int bitrate;
+
+    @property (nonatomic) AVAssetWriter *writer;
+    @property BOOL encounteredFirstBuffer;
+    @property CMSampleBufferRef afterAppBackgroundAudioSampleBuffer;
+    @property CMSampleBufferRef afterAppBackgroundMicSampleBuffer;
+    @property CMSampleBufferRef afterAppBackgroundVideoSampleBuffer;
 
 @end
